aac7265b607f67e3517ebce8ffd895e6836f57c1
DouglasBruce <douglasbruce96@gmail.com>
2019-04-18 15:25:43 +0100

    Commiting finished project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBXT87m4oPj6XGZuIL0uHQgLtLUMxSZrto",
-    authDomain: "firestorechat-7e6ce.firebaseapp.com",
-    databaseURL: "https://firestorechat-7e6ce.firebaseio.com",
-    projectId: "firestorechat-7e6ce",
-    storageBucket: "firestorechat-7e6ce.appspot.com",
-    messagingSenderId: "621266250730"
+    
   }
 };
 
